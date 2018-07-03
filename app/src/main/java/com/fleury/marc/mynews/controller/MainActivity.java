72d50e9f26559b057f374273d742dc13678c4ebd
Marc <marc-fleury@outlook.fr>
2018-07-03 14:16:30 +0200

    Initialization

---
+++ app/src/main/java/com/fleury/marc/mynews/controller/MainActivity.java
@@ -0,0 +1,15 @@
+package com.fleury.marc.mynews.controller;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.fleury.marc.mynews.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
